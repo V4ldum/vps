fbd67f731c4b3aca0f760758b4fc50bab38f86f4
V4ldum <56064897+V4ldum@users.noreply.github.com>
2026-09-02 11:56:07 +0200

    yey this works

--- dns.sh
+++ dns.sh
@@ -6,19 +6,13 @@ then
     exit 1;
 fi
 
-# Create dns64 interface
-cat > /etc/systemd/network/100-dns64.netdev <<'EOF'
-[NetDev]
-Name=dns64
-Kind=dummy
-EOF
+# ----------------------
+# Systemd-resolved
 
-# Create dns64 network
-cat > /etc/systemd/network/100-dns64.network <<'EOF'
-[Match]
-Name=dns64
-
-[Network]
+# Create dns64 config
+mkdir -p /etc/systemd/resolved.conf.d/
+cat > /etc/systemd/resolved.conf.d/dns64.conf <<'EOF'
+[Resolve]
 # Germany
 DNS=2a01:4f8:c2c:123f::1
 # Netherland
@@ -29,20 +23,10 @@ DNS=2a00:1098:2c::1
 DNS=2a01:4f9:c010:3f02::1
 # USA
 DNS=2a01:4ff:f0:9876::1
-Address=fd00:64::1/128
 Domains=~ghcr.io ~github.com ~githubusercontent.com
-DNSSEC=no
-LinkLocalAddressing=no
-IPv6AcceptRA=no
-ConfigureWithoutCarrier=yes
-
-[Link]
-ActivationPolicy=always-up
-
-[DHCP]
-UseDNS=no
 EOF
 
+
 # Restart resolved
-systemctl restart systemd-networkd systemd-resolved
+systemctl restart systemd-resolved
 resolvectl flush-caches
